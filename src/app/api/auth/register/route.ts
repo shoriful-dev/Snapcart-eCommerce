3ae0cf28fffd57d41082a071b9e52bd32c5a5b9b
shoriful-dev <cmshohelrana50@gmail.com>
2026-01-11 15:01:14 +0600

    add next-auth creadentials

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,1 +1,40 @@
+import connectDB from "@/lib/db";
+import User from "@/models/user.model";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
 
+// Register User
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+    const {name, email, password} = await req.json();
+    const existUser = await User.findOne({email});
+    if(existUser){
+      return NextResponse.json(
+        {message: 'Email Already exist!, try another email'},
+        {status: 400}
+      )
+    }
+    if(password.length < 6){
+      return NextResponse.json(
+        { message: 'Password must be at least 6 characters' },
+        { status: 400 }
+      );
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    })
+    return NextResponse.json(
+      user,
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: `Register failed! ${error}` },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/auth.ts
@@ -0,0 +1,64 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import connectDB from './lib/db';
+import User from './models/user.model';
+import bcrypt from 'bcryptjs';
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        await connectDB();
+        const email = credentials.email;
+        const password = credentials.password as string;
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw new Error('User Does Not Exist!');
+        }
+        const isMatchPassword = await bcrypt.compare(password, user.password);
+        if (!isMatchPassword) {
+          throw new Error('Incorrect Password, try again!');
+        }
+        return {
+          id: user._id.toString(),
+          name: user.name,
+          email: user.email,
+          role: user.role,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.name = user.name;
+        token.email = user.email;
+        token.role = user.role;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        session.user.name = token.name as string;
+        session.user.email = token.email as string;
+        session.user.role = token.role as string;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: '/login',
+    error: '/login',
+  },
+  session: {
+    strategy: 'jwt',
+    maxAge: 10 * 24 * 60 * 60 * 1000,
+  },
+  secret: process.env.AUTH_SECRET,
+});
