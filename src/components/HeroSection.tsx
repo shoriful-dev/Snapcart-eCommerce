21b7ccbc454a42aa65a33125e6de1420eba3de29
shoriful-dev <cmshohelrana50@gmail.com>
2026-01-29 23:18:43 +0600

    add banner animation with fremer motion

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Leaf, Smartphone, Truck } from 'lucide-react';
+import { Leaf, ShoppingBasket, Smartphone, Truck } from 'lucide-react';
 import { AnimatePresence, motion } from 'motion/react';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
@@ -48,22 +48,49 @@ const HeroSection = () => {
     return () => clearInterval(timer)
   },[])
   return (
-    <div className='relative w-[98%] mx-auto mt-32 h-[80vh] rounded-3xl overflow-hidden shadow-2xl'>
-      <AnimatePresence mode='wait'>
+    <div className="relative w-[98%] mx-auto mt-32 h-[80vh] rounded-3xl overflow-hidden shadow-2xl">
+      <AnimatePresence mode="wait">
         <motion.div
-        key={current}
-        initial={{opacity: 0}}
-        animate={{opacity: 1}}
-        transition={{duration: 0.8}}
-        exit={{opacity: 0}}
-        className='absolute inset-0'
+          key={current}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8 }}
+          exit={{ opacity: 0 }}
+          className="absolute inset-0"
         >
-          <Image src={slides[current]?.bg} alt='banner Image' fill priority className='object-cover'/>
-          <div className='absolute inset-0 bg-black/50 backdrop:blur-[1px]'/>
+          <Image
+            src={slides[current]?.bg}
+            alt="banner Image"
+            fill
+            priority
+            className="object-cover"
+          />
+          <div className="absolute inset-0 bg-black/50 backdrop:blur-[1px]" />
         </motion.div>
       </AnimatePresence>
+      <div className="absolute inset-0 flex items-center justify-center text-center text-white px-6">
+        <motion.div
+          initial={{ y:30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6 }}
+          className='flex flex-col items-center justify-center gap-6 max-w-3xl'
+        >
+          <div className="bg-white/10 backdrop:blur-md p-6 rounded-full shadow-lg">{slides[current].icon}</div>
+          <h1 className='text-3xl sm:text-5xl md:text-6xl font-extrabold tracking-tight drop-shadow-lg'>{slides[current].title}</h1>
+          <p className='text-lg sm:text-xl text-gray-200 max-w-2xl'>{slides[current].subTitle}</p>
+          <motion.button
+          whileHover={{scale: 1.09}}
+          whileTap={{scale: 0.96}}
+          transition={{duration: 0.2}}
+          className='mt-4 bg-white text-green-700 hover:bg-green-100 px-8 py-3 rounded-full font-semibold shadow-lg transition-all duration-300 flex items-center gap-2'
+          >
+            <ShoppingBasket className='w-5 h-5'/>
+            {slides[current].btnText}
+          </motion.button>
+        </motion.div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default HeroSection;
